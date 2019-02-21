43b366a5bc592bdb26737ca9c1dbb4579d6c4a42
Franklin Parker <fparker@deliveryman.com>
2019-02-21 17:57:53 -0500

    dynamic products

--- src/app/products/components/product-home/product-home.component.ts
+++ src/app/products/components/product-home/product-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsService} from '../../services/products.service';
+import {Product} from '../../models/product';
 
 @Component({
   selector: 'app-product-home',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-home.component.scss']
 })
 export class ProductHomeComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductsService) { }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
 
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductHomeComponent } from './components/product-home/product-home.component';
+import { ProductItemComponent } from './components/product-home/product-item/product-item.component';
 
 @NgModule({
-  declarations: [ProductHomeComponent],
+  declarations: [ProductHomeComponent, ProductItemComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
