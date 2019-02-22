5a521c8e84bb613745635d35c64e18657ebeeb86
Franklin Parker <fparker@deliveryman.com>
2019-02-21 19:25:22 -0500

    home no longer needed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
 import {EditItemComponent} from './components/edit-item/edit-item.component';
 import {LoginComponent} from './auth/components/login.component';
 import {AuthGuard} from './auth/services/auth.guard';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [AuthGuard]
-  },
+
   {
     path: '',
     loadChildren: './products/products.module#ProductsModule'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 import {MdlModule} from '@angular-mdl/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent} from './components/home/home.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { EditItemComponent } from './components/edit-item/edit-item.component';
-import {ReactiveFormsModule} from '@angular/forms';
 import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     TopNavComponent,
     EditItemComponent
   ],
