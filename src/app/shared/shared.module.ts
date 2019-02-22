5b7842267c6f233b4d078aac3ecdc7f3df58fad2
Franklin Parker <fparker@deliveryman.com>
2019-02-22 15:10:58 -0500

    use the mdl text field

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import {MdlModule} from '@angular-mdl/core';
 
 @NgModule({
   declarations: [],
@@ -8,7 +9,8 @@ import {ReactiveFormsModule} from '@angular/forms';
     CommonModule
   ],
   exports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MdlModule
   ]
 })
 export class SharedModule { }
