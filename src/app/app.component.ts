c770c3151e14f4b6738b272c53a009d45a8c68f5
Franklin Parker <fparker@deliveryman.com>
2019-02-24 18:36:20 -0500

    remove routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 
 import {AuthService} from './auth/services/auth.service';
-import {routeStateTrigger} from './routingAnimations';
 import {StitchService} from './shared/services/stitch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    routeStateTrigger
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   constructor(private authService: AuthService,
@@ -20,14 +15,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.authService.attemptLogin();
-  }
-
-  getAnimationData(outlet: RouterOutlet) {
-    const routeData = outlet.activatedRouteData['animation'];
-    if (!routeData) {
-      return 'rootPage';
-    }
-    return routeData['page'];
   }
 }
 
